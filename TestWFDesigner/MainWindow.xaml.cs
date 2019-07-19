283228372da59c279b69828760738c78640b3939
Bruno Costa <brunoazev.c@gmail.com>
2019-07-18 23:17:18 -0500

    Initial commit #TestWFDesigner
    Testing the design created.

---
+++ TestWFDesigner/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Activities.Core.Presentation;
+using System.Activities.Presentation;
+using System.Activities.Presentation.Metadata;
+using System.Activities.Presentation.Toolbox;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWFDesigner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            // register metadata
+            (new DesignerMetadata()).Register();
+            RegisterCustomMetadata();
+            // add custom activity to toolbox
+            Toolbox.Categories.Add(new ToolboxCategory("Custom activities"));
+            Toolbox.Categories[1].Add(new ToolboxItemWrapper(typeof(Statistical.R.RScope)));
+            Toolbox.Categories[1].Add(new ToolboxItemWrapper(typeof(Statistical.R.RunRScript)));
+            // create the workflow designer
+            WorkflowDesigner wd = new WorkflowDesigner();
+            wd.Load(new Sequence());
+            DesignerBorder.Child = wd.View;
+            PropertyBorder.Child = wd.PropertyInspectorView;
+
+        }
+
+        void RegisterCustomMetadata()
+        {
+            AttributeTableBuilder builder = new AttributeTableBuilder();
+            builder.AddCustomAttributes(typeof(Statistical.R.RScope), new DesignerAttribute(typeof(Statistical.R.Design.RScopeDesigner)));
+            builder.AddCustomAttributes(typeof(Statistical.R.RunRScript), new DesignerAttribute(typeof(Statistical.R.Design.RunRScriptDesigner)));
+            MetadataStore.AddAttributeTable(builder.CreateTable());
+        }
+    }
+}
